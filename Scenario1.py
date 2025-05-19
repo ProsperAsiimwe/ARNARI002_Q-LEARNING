c070200db0dbbbcc671073715439f0a5f161b337
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2025-05-19 11:32:36 +0200

    add plots and logs files in the code

--- Scenario1.py
+++ Scenario1.py
@@ -3,6 +3,7 @@ import random
 from FourRooms import FourRooms
 import sys
 import matplotlib.pyplot as plt
+import os
 
 # Parameters
 EPISODES = 500
@@ -39,41 +40,49 @@ def main():
     stochastic = '-stochastic' in sys.argv
     fourRoomsObj = FourRooms('simple', stochastic=stochastic)
 
-    episode_rewards = []
-    epsilon = EPSILON_START
+    # Create log and plot directories
+    os.makedirs("logs", exist_ok=True)
+    os.makedirs("plots", exist_ok=True)
 
-    for episode in range(EPISODES):
-        fourRoomsObj.newEpoch()
-        state = get_state(fourRoomsObj)
-        total_reward = 0
+    log_file_path = os.path.join("logs", "scenario1_log.txt")
+    with open(log_file_path, "w") as log_file:
+        episode_rewards = []
+        epsilon = EPSILON_START
 
-        while not fourRoomsObj.isTerminal():
-            action = select_action(state, epsilon)
-            cell_type, new_pos, packages_left, is_terminal = fourRoomsObj.takeAction(action)
-            next_state = get_state(fourRoomsObj)
-            reward = get_reward(cell_type, is_terminal)
+        for episode in range(EPISODES):
+            fourRoomsObj.newEpoch()
+            state = get_state(fourRoomsObj)
+            total_reward = 0
 
-            if next_state not in Q:
-                Q[next_state] = np.zeros(len(ACTIONS))
+            while not fourRoomsObj.isTerminal():
+                action = select_action(state, epsilon)
+                cell_type, new_pos, packages_left, is_terminal = fourRoomsObj.takeAction(action)
+                next_state = get_state(fourRoomsObj)
+                reward = get_reward(cell_type, is_terminal)
 
-            best_next_action = np.max(Q[next_state])
-            Q[state][action] += ALPHA * (reward + GAMMA * best_next_action - Q[state][action])
-            state = next_state
-            total_reward += reward
+                if next_state not in Q:
+                    Q[next_state] = np.zeros(len(ACTIONS))
 
-        episode_rewards.append(total_reward)
-        epsilon = max(EPSILON_MIN, epsilon * EPSILON_DECAY)
-        print(f"Episode {episode+1}/{EPISODES}, Total Reward: {total_reward}, Epsilon: {epsilon:.3f}")
+                best_next_action = np.max(Q[next_state])
+                Q[state][action] += ALPHA * (reward + GAMMA * best_next_action - Q[state][action])
+                state = next_state
+                total_reward += reward
+
+            episode_rewards.append(total_reward)
+            epsilon = max(EPSILON_MIN, epsilon * EPSILON_DECAY)
+            log_line = f"Episode {episode+1}/{EPISODES}, Total Reward: {total_reward}, Epsilon: {epsilon:.3f}\n"
+            print(log_line.strip())
+            log_file.write(log_line)
 
     # Save and show path for last run
-    fourRoomsObj.showPath(-1, savefig="scenario1_path.png")
+    fourRoomsObj.showPath(-1, savefig="./plots/scenario1_path.png")
 
     # Plot rewards
     plt.plot(episode_rewards)
     plt.title("Scenario 1: Episode Rewards")
     plt.xlabel("Episode")
     plt.ylabel("Total Reward")
-    plt.savefig("scenario1_rewards.png")
+    plt.savefig("./plots/scenario1_rewards.png")
     plt.close()
 
 if __name__ == "__main__":
